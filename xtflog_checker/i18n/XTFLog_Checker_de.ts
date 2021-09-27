8672d708ee573919ce1b8fb7e7b4ee5b4f5ef202
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2021-09-27 14:11:18 +0200

    Show warning if file is large or contains no error

--- xtflog_checker/i18n/XTFLog_Checker_de.ts
+++ xtflog_checker/i18n/XTFLog_Checker_de.ts
@@ -4,64 +4,84 @@
 <context>
     <name>generals</name>
     <message>
-        <location filename="../XTFLog_Checker_dock_panel.py" line="47"/>
+        <location filename="../XTFLog_Checker_dock_panel.py" line="44"/>
         <source>Show errors</source>
         <translation>Fehler anzeigen</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dock_panel.py" line="48"/>
+        <location filename="../XTFLog_Checker_dock_panel.py" line="45"/>
         <source>Show warnings</source>
         <translation>Warnungen anzeigen</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="46"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="45"/>
         <source>Create layer</source>
         <translation>Layer erstellen</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="49"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="48"/>
         <source>Cancel</source>
         <translation>Abbrechen</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="52"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="51"/>
         <source>Show error log</source>
         <translation>Fehlerprotokoll anzeigen</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="53"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="52"/>
         <source>Upload xtf-log file</source>
         <translation>Pfad oder URL einer XTF-Error-Log Datei auswählen</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="54"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="53"/>
         <source>Show log for existing layer</source>
         <translation>Fehler-Log für vorhandenen Layer anzeigen</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="55"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="54"/>
         <source>Only layers created with this plugin can be selected</source>
         <translation>Nur mit diesem Plugin erstellte Layer können ausgewählt werden</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="91"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="94"/>
         <source>No valid file</source>
         <translation>Keine gültige Datei</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="85"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="86"/>
         <source>Could not get a valid XTF-Log file from specified Url</source>
         <translation>Es konnte keine gültige XTF-Log-Datei von der angegebenen URL abgerufen werden</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="91"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="94"/>
         <source>No valid XTF-Log file at specified Path</source>
         <translation>Keine gültige XTF-Log-Datei unter angegebenem Pfad</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dock_panel.py" line="51"/>
+        <location filename="../XTFLog_Checker_dock_panel.py" line="48"/>
         <source>Error log</source>
         <translation>XTFLog-Checker Fehlerprotokoll</translation>
+    </message>
+    <message>
+        <location filename="../XTFLog_Checker_dialog.py" line="155"/>
+        <source>No Errors</source>
+        <translation>Keine Fehler</translation>
+    </message>
+    <message>
+        <location filename="../XTFLog_Checker_dialog.py" line="155"/>
+        <source>The selected XTF file contains no Ilivalidator-Errors, select another file.</source>
+        <translation>Die ausgewählte Datei enthält keine Fehler vom Typ IliVErrors, wählen Sie eine andere Datei.</translation>
+    </message>
+    <message>
+        <location filename="../XTFLog_Checker_dialog.py" line="90"/>
+        <source>Large file</source>
+        <translation>Datei grösser als 5 MB</translation>
+    </message>
+    <message>
+        <location filename="../XTFLog_Checker_dialog.py" line="90"/>
+        <source>Processing of large XTF-Log files might take a while</source>
+        <translation>Die Verarbeitung grosser Dateien kann eine Weile dauern</translation>
     </message>
 </context>
 </TS>
--- xtflog_checker/i18n/XTFLog_Checker_fr.ts
+++ xtflog_checker/i18n/XTFLog_Checker_fr.ts
@@ -4,64 +4,84 @@
 <context>
     <name>generals</name>
     <message>
-        <location filename="../XTFLog_Checker_dock_panel.py" line="47"/>
+        <location filename="../XTFLog_Checker_dock_panel.py" line="44"/>
         <source>Show errors</source>
         <translation>Afficher erreures</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dock_panel.py" line="48"/>
+        <location filename="../XTFLog_Checker_dock_panel.py" line="45"/>
         <source>Show warnings</source>
         <translation>Afficher avertissements</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="46"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="45"/>
         <source>Create layer</source>
         <translation>Créer layer</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="49"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="48"/>
         <source>Cancel</source>
         <translation>Annuler</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="52"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="51"/>
         <source>Show error log</source>
         <translation>Afficher le log des erreurs</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="53"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="52"/>
         <source>Upload xtf-log file</source>
         <translation>Choisir chemin ou URL d&apos;un fichier XTF-Error-Log</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="54"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="53"/>
         <source>Show log for existing layer</source>
         <translation>Afficher le log des erreurs pour un layer existant</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="55"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="54"/>
         <source>Only layers created with this plugin can be selected</source>
         <translation>Seuls les layers créés avec ce plugin peuvent être sélectionnés</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="91"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="94"/>
         <source>No valid file</source>
         <translation>Fichier non valide</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="85"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="86"/>
         <source>Could not get a valid XTF-Log file from specified Url</source>
         <translation>Impossible d&apos;obtenir un fichier XTF-Log valide à partir de l&apos;URL spécifiée</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dialog.py" line="91"/>
+        <location filename="../XTFLog_Checker_dialog.py" line="94"/>
         <source>No valid XTF-Log file at specified Path</source>
         <translation>Aucun fichier XTF-Log valide au chemin d&apos;accès spécifié</translation>
     </message>
     <message>
-        <location filename="../XTFLog_Checker_dock_panel.py" line="51"/>
+        <location filename="../XTFLog_Checker_dock_panel.py" line="48"/>
         <source>Error log</source>
         <translation>XTFLog-Checker log des erreurs</translation>
+    </message>
+    <message>
+        <location filename="../XTFLog_Checker_dialog.py" line="155"/>
+        <source>No Errors</source>
+        <translation>Aucune erreur</translation>
+    </message>
+    <message>
+        <location filename="../XTFLog_Checker_dialog.py" line="155"/>
+        <source>The selected XTF file contains no Ilivalidator-Errors, select another file.</source>
+        <translation>Le fichier sélectionné ne contient aucune erreur du type IliVErrors, sélectionnez un autre fichier.</translation>
+    </message>
+    <message>
+        <location filename="../XTFLog_Checker_dialog.py" line="90"/>
+        <source>Large file</source>
+        <translation>Fichier de plus de 5 MB</translation>
+    </message>
+    <message>
+        <location filename="../XTFLog_Checker_dialog.py" line="90"/>
+        <source>Processing of large XTF-Log files might take a while</source>
+        <translation>Le traitement de fichiers volumineux peut prendre un certain temps</translation>
     </message>
 </context>
 </TS>
